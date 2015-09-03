d4c1498ce40f7e43a9d3fd2635b6969f50297130
Michi <nerdkram@gmx.de>
2015-09-03 16:07:47 +0200

    PowerShell Scripts

---
+++ scripts/Windows10_Cleanup.ps1
@@ -0,0 +1,90 @@
+# Windows cleanup script
+# Michi Dönselmann
+# http://blog.doenselmann.com
+# 02.09.2015
+
+# Execution examples:
+# From command prompt: 			powershell.exe -ExecutionPolicy Bypass "& '.\WindowsCleanup.ps1'"
+# Within powershell console: 	.\WindowsCleanup.ps1
+
+#----------------------------------------------------------------------------------
+# global variables. Change values here if needed
+
+# variable for firewall profiles
+$tag = "StateFlags"
+$counter = "0001"
+$stateflags = $($tag) + $($counter)
+$options =  "Active Setup Temp Folders","BranchCache","Downloaded Program Files","Internet Cache Files","Old ChkDsk Files","Previous Installations","Recycle Bin","RetailDemo Offline Content","Service Pack Cleanup","Setup Log Files","System error memory dump files","System error minidump files","Temporary Files","Temporary Setup Files","Thumbnail Cache","Update Cleanup","Upgrade Discarded Files","User file versions","Windows Defender","Windows Error Reporting Archive Files","Windows Error Reporting Queue Files","Windows Error Reporting System Archive Files","Windows Error Reporting System Queue Files","Windows Error Reporting Temp Files","Windows ESD installation files","Windows Upgrade Log Files"
+
+
+#--------------------------------
+#								-
+#   STOP CHANGING THINGS NOW    -
+#								-
+#--------------------------------
+
+#----------------------------------------------------------------------------------
+
+# global error handling
+$ErrorActionPreference = "SilentlyContinue";
+$error.Clear();
+# only show defined error messages
+function errorhandling($message = $error[0].Exception.Message, $Code)
+{
+	# write execution Log to %tempp%
+	$error | out-file -PSPath $env:temp\PowerShell_Execution_Error.log -Append;
+	# on error just show content of Exeption.Message
+	$message = "Error executing section $($code): " + $($message)
+	write-warning $($message)
+	# stop script on error
+	read-host
+	exit $code;
+}
+
+#----------------------------------------------------------------------------------
+# check if script runs in admin context
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+	[Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    Write-Warning "You have to run this Script with elevated privileges to delete all files"
+    Write-Host "Press any key to exit the script"
+    read-host
+	exit 0;
+}
+
+#----------------------------------------------------------------------------------
+# set cleanup registiry keys
+
+# reset options if values are changed
+$rootpath = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\"	
+$keys = Get-ChildItem -path $rootpath -Recurse | Get-ItemProperty -name $stateflags -ErrorAction SilentlyContinue
+
+foreach($key in $keys.pspath)
+{
+	
+	Set-ItemProperty -path $key -name $stateflags -value "0" -Type Dword
+
+}
+
+#define options
+foreach($option in $options)
+{
+	$path = $rootpath + $option
+	Set-ItemProperty -path $path -name $stateflags -value "2" -Type Dword
+
+}
+
+#----------------------------------------------------------------------------------
+# define cleanup variables
+$error.Clear();
+$prog = "$env:SystemRoot\System32\cleanmgr.exe"
+$param = "/sagerun:$counter /setup"
+
+# start cleanup process
+Invoke-Expression "$prog $param"
+	
+	# errorhandling
+	if ($error.Count -gt 0)
+	{
+		errorhandling -code 1;
+	}
---
+++ scripts/Windows10_Privacy.ps1
@@ -0,0 +1,180 @@
+# Windows 10 privacy settings
+# Michi Dönselmann
+# http://blog.doenselmann.com
+# 27.08.2015
+
+# Execution examples:
+# Enable privacy protection: powershell.exe -ExecutionPolicy Bypass "& '.\Windows10_Privacy.ps1 ' -enable:$true"
+# Disable privacy protection: powershell.exe -ExecutionPolicy Bypass "& '.\Windows10_Privacy.ps1 ' -enable:$false"
+
+# define script execution params. Default is false, so spying will stay enabled
+param
+(
+    [bool]$enable = $false
+)
+
+#----------------------------------------------------------------------------------
+# global variables. Change values here if needed
+
+# variable for services
+$services =  "diagtrack","dmwappushservice"
+# variable for sheduled tasks
+$tasks =  "Microsoft Compatibility Appraiser","ProgramDataUpdater","Consolidator","KernelCeipTask","UsbCeip"
+
+#--------------------------------
+#								-
+#   STOP CHANGING THINGS NOW    -
+#								-
+#--------------------------------
+
+#----------------------------------------------------------------------------------
+
+# global error handling
+# only show defined error messages
+$ErrorActionPreference = "SilentlyContinue";
+$error.Clear();
+
+function errorhandling($message = $error[0].Exception.Message, $Code)
+{
+	# write execution Log to %tempp%
+	$error | out-file -PSPath $env:temp\PowerShell_Execution_Error.log -Append;
+	# on error just show content of Exeption.Message
+	$message = "Error executing section $($code): " + $($message)
+	write-warning $($message)
+	# stop script on error
+	read-host
+	exit $code;
+}
+
+#----------------------------------------------------------------------------------
+# get system architecture
+$arch = (Get-Process -Id $PID).StartInfo.EnvironmentVariables["PROCESSOR_ARCHITECTURE"];
+if ($arch -eq "AMD64")
+{
+	$arch = "x64"
+}
+else
+{
+	$arch = "x86"
+}
+
+#----------------------------------------------------------------------------------
+# check if script runs in admin context
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+	[Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    Write-Warning "You have to run this Script with elevated privileges"
+    Write-Host "Press any key to exit the script"
+    read-host
+	exit 0;
+}
+
+#--------------------------------
+#				                - 
+#   enable privacy protection   -
+#                  		        -
+#--------------------------------
+
+if ($enable -eq "True")
+{
+
+	#----------------------------------------------------------------------------------
+	# disable data collecting services
+	foreach($a in $services)
+	{
+		set-service $a -startuptype disabled
+
+		# errorhandling
+		if ($error.Count -gt 0)
+		{
+			errorhandling -code 3;
+		}	
+	}
+
+	#----------------------------------------------------------------------------------
+	# diable telemetry (also possible with GPO)
+	$path = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection"
+	Set-ItemProperty -path $path -name AllowTelemetry -value "1"
+		
+		# only needed on x64 systems
+		if ($arch -eq "x64")
+		{
+			$path = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Policies\DataCollection"
+			Set-ItemProperty -path $path -name AllowTelemetry -value "1"
+		}
+		
+	# errorhandling
+	if ($error.Count -gt 0)
+	{
+		errorhandling -code 4;
+	}
+
+	#----------------------------------------------------------------------------------
+	# disable sheduled tasks
+	foreach($a in $tasks)
+	{
+		$task = Get-ScheduledTask $a;
+		Disable-ScheduledTask $task;
+		
+		# errorhandling
+		if ($error.Count -gt 0)
+		{
+			errorhandling -code 5;
+		}
+	}
+
+}
+else
+
+#--------------------------------
+#				                - 
+#   disable privacy protection  -
+#                 		        -
+#--------------------------------
+
+{
+	#----------------------------------------------------------------------------------
+	# enable data collecting services
+	foreach($a in $services)
+	{
+		set-service $a -startuptype automatic
+
+		# errorhandling
+		if ($error.Count -gt 0)
+		{
+			errorhandling -code 3;
+		}	
+	}
+
+	#----------------------------------------------------------------------------------
+	# enable telemetry (also possible with GPO)
+	$path = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection"
+	Set-ItemProperty -path $path -name AllowTelemetry -value "2"
+		
+		# only needed on x64 systems
+		if ($arch -eq "x64")
+		{
+			$path = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Policies\DataCollection"
+			Set-ItemProperty -path $path -name AllowTelemetry -value "2"
+		}
+		
+	# errorhandling
+	if ($error.Count -gt 0)
+	{
+		errorhandling -code 4;
+	}
+
+	#----------------------------------------------------------------------------------
+	# enable sheduled tasks
+	foreach($a in $tasks)
+	{
+		$task = Get-ScheduledTask $a;
+		Enable-ScheduledTask $task;
+		
+		# errorhandling
+		if ($error.Count -gt 0)
+		{
+			errorhandling -code 5;
+		}
+	}
+}
