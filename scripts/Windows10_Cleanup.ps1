cf9a431cdd427ef4dbd2358107741bbd2aacc338
Michi <MichiMunich@users.noreply.github.com>
2020-02-05 14:04:02 +0100

    Windows10_Cleanup.ps1
    Smaller enhancements and optimizations

--- scripts/Windows10_Cleanup.ps1
+++ scripts/Windows10_Cleanup.ps1
@@ -1,7 +1,7 @@
 # Windows cleanup script
 # Michi Dönselmann
 # https://blog.doenselmann.com
-# 02.11.2016
+# 05.02.2020
 
 # Execution examples:
 # From command prompt: powershell.exe -ExecutionPolicy Bypass "& '.\WindowsCleanup.ps1'"
@@ -14,13 +14,12 @@
 $tag = "StateFlags"
 $counter = "0001"
 $stateflags = $($tag) + $($counter)
-$options =  "Active Setup Temp Folders","BranchCache","Downloaded Program Files","Internet Cache Files","Old ChkDsk Files","Previous Installations","Recycle Bin","RetailDemo Offline Content","Service Pack Cleanup","Setup Log Files","System error memory dump files","System error minidump files","Temporary Files","Temporary Setup Files","Thumbnail Cache","Update Cleanup","Upgrade Discarded Files","User file versions","Windows Defender","Windows Error Reporting Archive Files","Windows Error Reporting Queue Files","Windows Error Reporting System Archive Files","Windows Error Reporting System Queue Files","Windows Error Reporting Temp Files","Windows ESD installation files","Windows Upgrade Log Files"
-
+$options =  "Active Setup Temp Folders","BranchCache","D3D Shader Cache","Delivery Optimization Files","Diagnostic Data Viewer database files","Downloaded Program Files","Internet Cache Files","Old ChkDsk Files","Previous Installations","Recycle Bin","RetailDemo Offline Content","Service Pack Cleanup","Setup Log Files","System error memory dump files","System error minidump files","Temporary Files","Temporary Setup Files","Thumbnail Cache","Update Cleanup","Upgrade Discarded Files","User file versions","Windows Defender","Windows Error Reporting Files","Windows ESD installation files","Windows Upgrade Log Files"
 
 #--------------------------------
-#								-
+#				-
 #   STOP CHANGING THINGS NOW    -
-#								-
+#				-
 #--------------------------------
 
 #----------------------------------------------------------------------------------
@@ -61,9 +60,7 @@ $keys = Get-ChildItem -path $rootpath -Recurse | Get-ItemProperty -name $statefl
 
 foreach($key in $keys.pspath)
 {
-	
 	Set-ItemProperty -path $key -name $stateflags -value "0" -Type Dword
-
 }
 
 #define options
@@ -71,7 +68,6 @@ foreach($option in $options)
 {
 	$path = $rootpath + $option
 	Set-ItemProperty -path $path -name $stateflags -value "2" -Type Dword
-
 }
 
 #----------------------------------------------------------------------------------
@@ -81,10 +77,10 @@ $prog = "$env:SystemRoot\System32\cleanmgr.exe"
 $param = "/sagerun:$counter /setup"
 
 # start cleanup process
-Invoke-Expression "$prog $param"
+start-Process -FilePath $prog -ArgumentList $param -wait -passthru
 	
-	# errorhandling
-	if ($error.Count -gt 0)
-	{
-		errorhandling -code 1;
-	}
+# errorhandling
+if ($error.Count -gt 0)
+{
+	errorhandling -code 1;
+}
